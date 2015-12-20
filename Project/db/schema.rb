34b30b72bc0036615708cf6cab208f0a29e1dc38
PeterHany <peter.h.alfy@gmail.com>
2015-12-20 14:46:07 +0200

    Project

--- Project/db/schema.rb
+++ Project/db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215183503) do
-
-  create_table "project_models", force: :cascade do |t|
-    t.string   "first_name", limit: 25
-    t.string   "last_name",  limit: 50
-    t.string   "email",      limit: 255, default: "", null: false
-    t.string   "password",   limit: 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
